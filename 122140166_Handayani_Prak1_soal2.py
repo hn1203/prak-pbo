46679851776603ac6fdeccddce1437e780308de4
Handayani <139942587+hn1203@users.noreply.github.com>
2024-02-28 17:51:10 +0700

    Add files via upload

---
+++ 122140166_Handayani_Prak1_soal2.py
@@ -0,0 +1,10 @@
+Jari=float(input("Jari-Jari: "))
+
+if Jari<0:
+    print ("Jari-jari lingkaran tidak boleh negatif")
+    
+luas= 3.14*Jari*Jari
+keliling=2*3.14*Jari
+print ("Luas: ", luas)
+print ("Keliling: ",keliling)
+    
